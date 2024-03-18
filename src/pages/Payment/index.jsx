e94993fe150fea76efc3f45b657631c270e8b322
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-03-18 20:53:36 -0300

    :lipstick: Style payment page

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,10 +1,71 @@
-import { Container } from "./style";
+import { Container, RequestList, StatusPayment } from "./style";
+import { PlateContext } from "../../hooks/plateRequest";
+import { useContext, useState} from "react";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import imageTeste from "../../assets/group-11.png"
+
+import { FaPix } from "react-icons/fa6";
+import { IoWalletOutline } from "react-icons/io5";
+import { IoCopy } from "react-icons/io5";
 
 export function Payment(){
+  const { plateRequest } = useContext(PlateContext);
+  const [methodSelect, setMethodSelect] = useState("pix");
+  console.log(methodSelect)
+
+
+  console.log(plateRequest)
 
   return(
     <Container>
-      
+      <Header/>
+
+      <div className="payment-container">
+        <RequestList>
+          <h2>Meus Pedidos</h2>
+
+          {plateRequest 
+          ? <div className="plate-content">
+            <img src={imageTeste} alt="" />
+            <div className="plate-info">
+                <div>
+                  <p>1 x</p>
+                  <p>Salada Radish</p>
+                  <p>R$ 25,99</p>
+                </div>
+                <button>Excluir</button>
+              </div>
+            </div> 
+          : <div className="plate-content">
+              <p>Nenhum Pedido Registrado</p>
+            </div>}
+
+          <h3>Total: R$ 103,00</h3>
+        </RequestList>
+
+        <StatusPayment>
+          <h2>Pagamento</h2>
+          <div className="select-payment">
+            <div>
+              <button 
+              onClick={() => setMethodSelect("pix") } 
+              className={methodSelect === "pix" ? "method-select" : null}>
+                {<FaPix size={32}/>} PIX
+              </button>
+              
+              <button 
+              onClick={() => setMethodSelect("credit")} 
+              className={methodSelect === "credit" ? "method-select" : null}>
+                {<IoWalletOutline size={32}/>} Crédito
+              </button>
+            </div>
+            <img src="https://images.unsplash.com/photo-1550482768-88b710a445fd?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <p>Copiar Código {<IoCopy />}</p>
+          </div>
+        </StatusPayment>
+      </div>
+      <Footer/>
     </Container>
   )
 };
--- src/pages/Payment/style.js
+++ src/pages/Payment/style.js
@@ -1,6 +1,127 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100dvh;
   width: 100%;
-`
+
+
+  .payment-container{
+    width: 110rem;
+    display: flex;
+    height: 100dvh;
+    margin: auto;
+    padding-top: 4rem;
+
+    p, h2, h3{
+    font-family: "Poppins";
+    }
+
+    h2{
+      font-size: 3.2rem;
+      font-weight: 500;
+    }
+
+    h3{
+      font-size: 2rem;
+      font-weight: 500;
+    }
+  }
+`;
+
+export const RequestList = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+
+
+  .plate-info{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-left: 2rem;
+
+    div{
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      p:nth-child(3){
+        color: darkgray;
+        font-size: 1.6rem;
+        font-family: "Roboto";
+      }
+    }
+
+    p{
+      font-size: 2rem;
+    }
+  }
+
+  .plate-content{
+    display: flex;
+    align-items: center;
+  }
+
+  button{
+    background: none;
+    text-align: start;
+    font-family: Roboto;
+    font-size: 1.4rem;
+    color: ${({theme}) => theme.COLORS.TOMATO_400}
+  }
+
+  img{
+    width: 7rem;
+  }
+`;
+
+export const StatusPayment = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+
+
+  .select-payment{
+    width: 500px;
+    border: 2px solid gray;
+    border-radius: 8px;
+
+    div{
+      display: flex;
+    }
+
+    button{
+      width: 50%;
+      background: none;
+      padding: 2rem 0;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      justify-content: center;
+      border-bottom: 2px solid gray;
+      transition: all 300ms ease-in-out;
+      
+      &:hover{
+        background-color: rgba(255, 255, 255, 0.6);
+        filter: brightness(1);
+      }
+    }
+
+    button + button{
+      border-left: 2px solid gray;
+    }
+
+    .method-select{
+      background-color: ${({theme}) => theme.COLORS.LIGHT_700};
+    }
+  }
+
+  img{
+    width: 25rem;
+    height: 25rem;
+    object-fit: cover;
+    display: flex;
+    margin: 4rem auto;
+  }
+`;
