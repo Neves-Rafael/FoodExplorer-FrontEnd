610c7ef69a9b98960158ba3aafd4379939ed31e8
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-03-27 14:13:20 -0300

    :sparkles: Feat orderHistory page version mobile

--- src/pages/OrderHistory/index.jsx
+++ src/pages/OrderHistory/index.jsx
@@ -3,16 +3,41 @@ import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { api } from "../../service/api";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ButtonText } from "../../components/ButtonText";
+import { FaArrowLeft } from "react-icons/fa6";
 
 
 export function OrderHistory(){
   const [historyOrder, setHistoryOrder] = useState([]);
+  const navigate = useNavigate();
+
+  function updateTimeToBrazil(timer){
+    let setData = new Date(timer);
+
+    function addZero(data){
+      if(data < 10){
+        data = "0" + data;
+      }
+      return data
+    }
+    
+    const day = addZero(setData.getDate());
+    const month = addZero(setData.getDate());
+    const hours = addZero(setData.getHours());
+    const minutes = addZero(setData.getMinutes());
+
+    return `${day}/${month} às ${hours}h${minutes}`
+
+  }
 
   useEffect(()=> {
     async function searchMyOrders(){
       const myOrder = await api.get("/payment");
       setHistoryOrder(myOrder.data);
-      console.log(myOrder.data[0].id)
+      const daleee = JSON.parse(myOrder.data[5].plates);
+      console.log(daleee[1].quantity)
+      console.log(daleee[1].plate.name)
     }
 
     searchMyOrders();
@@ -22,21 +47,55 @@ export function OrderHistory(){
     <Container>
       <Header/>
       <main>
-        <h2>Pedidos</h2>
+        <button onClick={()=> navigate(-1)}>{<FaArrowLeft size={24}/>} Voltar</button>
+        <h2>Histórico de Pedidos</h2>
         <MobileContent>
-          {historyOrder && historyOrder.map((order)=> (<div className="request-content">
+          {historyOrder && historyOrder.map((order, index)=> (
+          <div className="request-content" key={index} onClick={() => navigate(`/payment/${order.id}`)}>
             <div className="data-info">
-              <p>{order.id}</p>
-              <p>Pendente</p>
-              <p>20/05 às 18h00</p>
+              <p>Nº {order.id}</p>
+              <p className="color-status">
+                {order.status === "pendente" && <span className="orange"/>}
+                {order.status === "finalizado" && <span className="green"/>}
+                {order.status === "cozinha" && <span className="blue"/>}
+                {order.status === "em andamento" && <span className="light-blue"/>}
+                {order.status === "cancelado" && <span className="red"/>}
+                {order.status}
+              </p>
+              <p>{updateTimeToBrazil(order.updated_at)}</p>
             </div>
-            <p className="plate-info">
-              1x salada Radish
-            </p>
+            <div className="plate-info">
+              {JSON.parse(order.plates).map((plate, index)=>(
+                <p key={index}>
+                  {`${plate.quantity} x`} {plate.plate.name}  
+                </p>
+              ))}
+            </div>
           </div>))}
         </MobileContent>
         <DesktopContent>
-
+        {historyOrder && historyOrder.map((order, index)=> (
+          <div className="request-content" key={index} onClick={() => navigate(`/payment/${order.id}`)}>
+            <div className="data-info">
+              <p>Nº {order.id}</p>
+              <p className="color-status">
+                {order.status === "pendente" && <span className="orange"/>}
+                {order.status === "finalizado" && <span className="green"/>}
+                {order.status === "cozinha" && <span className="blue"/>}
+                {order.status === "em andamento" && <span className="light-blue"/>}
+                {order.status === "cancelado" && <span className="red"/>}
+                {order.status}
+              </p>
+              <p>{updateTimeToBrazil(order.updated_at)}</p>
+            </div>
+            <div className="plate-info">
+              {JSON.parse(order.plates).map((plate, index)=>(
+                <p key={index}>
+                  {`${plate.quantity} x`} {plate.plate.name}  
+                </p>
+              ))}
+            </div>
+          </div>))}
         </DesktopContent>
       </main>
       <Footer/>
--- src/pages/OrderHistory/style.js
+++ src/pages/OrderHistory/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-
+  
   main{
     min-height: calc(100dvh - 200px);
 
@@ -10,13 +10,32 @@ export const Container = styled.div`
       font-family: "Poppins";
       font-weight: 500;
       font-size: 3.2rem;
-      margin: 6rem 2rem 2rem;
+      margin: 2rem 2rem 4rem;
+    }
+
+    button{
+      font-family: "Poppins";
+      font-weight: 500;
+      font-size: 2rem;
+      margin: 6rem 2rem;
+      background: none;
+      display: flex;
+      align-items: center;
+      gap: 1rem
     }
   }
 `;
 
 export const MobileContent =  styled.div`
   margin: 2rem;
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+  justify-content: center;
+  
+  @media(min-width: 768px){
+    display: none;
+  }
 
   .request-content{
     border: 2px solid darkgray;
@@ -25,15 +44,62 @@ export const MobileContent =  styled.div`
     display: flex;
     flex-direction: column;
     gap: 2rem;
+    width: 100%;
+    max-width: 30rem;
+    cursor: pointer;
+    transition: all ease-in-out 200ms;
+
+    &:hover{
+      background-color: darkslategrey;
+    }
   }
 
-  p{
+  p, span{
     font-size: 1.4rem;
   }
 
   .data-info{
     display: flex;
     justify-content: space-between;
+  }
+
+  .plate-info{
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr 1fr;
+    text-transform: capitalize;
+  }
+
+  .color-status{
+    display: flex;
+    align-items: center;
+    gap: .8rem;
+    text-transform: capitalize;
+
+    span{
+      height: .8rem;
+      width: .8rem;
+      border-radius: 100%;
+    }
+    .red{
+      background-color: red;
+    }
+
+    .green{
+      background-color: green;
+    }
+
+    .blue{
+      background-color: blueviolet;
+    }
+
+    .orange{
+      background-color: orange;
+    }
+
+    .light-blue{
+      background-color: lightseagreen;
+    }
   }
 `;
 
